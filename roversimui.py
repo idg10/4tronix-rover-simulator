4dafce910383d8b6175c1a761088e31054bb7892
Ian Griffiths <Ian.Griffiths@endjin.com>
2023-05-16 17:56:51 +0100

    Added design notes for planned HTTP interface for simulator UI

--- roversimui.py
+++ roversimui.py
@@ -1,3 +1,54 @@
+# 4tronix M.A.R.S. Rover Simulator UI
+#
+# This displays a window representing the rover. It keeps track of its position
+# and speed, and the orientation of its wheels. It models this in real time -
+# if the virtual rover is set in motion, it will continue to move until further
+# instructions are sent telling it to stop.
+#
+# This receives incoming HTTP requests to control the rover. Incoming messages
+# are in JSON form. The following properties may be set in the top-level
+# message:
+#   wheelMotors
+#   servos
+#   rgbLeds
+#
+# A request setting both might look like this:
+# {
+#   "wheelMotors": {
+#     "l": [ 100, 0 ],
+#     "r": [ 0, 100 ]
+#   },
+#   "servos": {
+#     "0": 0,
+#     "15": -10
+#   },
+#   "rgbLeds": {
+#     "0": [255, 0, 0],
+#     "3": [0, 255, 255]
+#   }
+# }
+#
+# The wheelMotors settings reflect the way the board itself is designed: it
+# seems that there are separate PWM outputs for each direction. The duty
+# cycle is set directly to the speed. More cryptically, the frequency is also
+# adjusted with the speed. Maximum speed is 100, and the frequency is the speed
+# divided by 2 in Hz. So at maximum speed, the PWM frequency is 50Hz. At half
+# speed it's 25Hz, and so on until we get to a floor of 10Hz. However, we're
+# not simulating at that level of detail. We're just accepting the speeds in
+# each direction, and setting the speed to 100 in both directions actively
+# brakes the wheels (whereas setting both speeds to zero lets the motor coast
+# naturally to a halt, which it does pretty quickly).
+# There are 16 servo outputs, although with a standard setup only 5 are used.
+# Any servos not specified in the message will not have their positions
+# changed.
+#
+# The response is always of the same format (even if the request is empty):
+# {
+#   "ultrasonicRange": 80
+# }
+#
+# This reports the detected range from the ultrasonic sensor.
+   
 import sys
 import json
 
@@ -7,6 +58,7 @@ from PyQt6.QtGui import QPixmap, QTransform
 
 from flask import Flask, request
 
+# Receives requ
 class ServerWorker(QObject):
     mysignal = pyqtSignal(str)
     http_server = Flask("RoverSimUi")
@@ -15,7 +67,6 @@ class ServerWorker(QObject):
         self.http_server.route('/', methods=['POST'])(self.result)
         self.http_server.run(port=8523)
 
-    #@http_server.route('/', methods=['POST'])
     def result(self): #, *args, **kwargs):
         bodyText = json.dumps(request.json)
         print(request.data)
